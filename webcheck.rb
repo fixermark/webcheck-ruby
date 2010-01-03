31495dfbcbb7a69c73084d5868fb9c114867e414
Mark T. Tomczak <iam@fixermark.com>
2010-01-03 11:21:13 -0500

    Initial checkin. Script can find anchors but not do anything with them.

---
+++ webcheck.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+require 'open-uri'
+$: << "gems/gems/nokogiri-1.4.1-x86-mswin32/lib"
+
+require 'nokogiri'
+
+doc = Nokogiri::HTML(
+    open('http://foundation.kappachapter.org/index.html')
+)
+
+doc.css('a').each do |link|
+  puts link.content
+end
+
